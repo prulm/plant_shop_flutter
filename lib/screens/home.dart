8ed587e1f2d6e87e11f7a39cae291c0910ee9027
Prulm <jaredyared83@gmail.com>
2023-10-31 22:16:17 +0300

    app bar action button added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:plant_shop_flutter/screens/plants.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: TextButton(
+                child: Text('Skip'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: ((context) => Plants()),),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
   }
 }
