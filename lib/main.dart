1da438809f8fecb245864ff0817ebc8dda730e33
Prulm <jaredyared83@gmail.com>
2023-10-31 22:06:55 +0300

    debug mode turned off

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class PlantShop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Home(),
     );
   }
