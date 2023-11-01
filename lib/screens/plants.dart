676a3dcb2e19aa6ae47c1975a1066f7fe04353bf
Prulm <jaredyared83@gmail.com>
2023-11-01 15:10:43 +0300

    app bar

--- lib/screens/plants.dart
+++ lib/screens/plants.dart
@@ -10,6 +10,32 @@ class Plants extends StatefulWidget {
 class _PlantsState extends State<Plants> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Search Products",
+          style: TextStyle(
+            color: Colors.grey[900],
+            fontSize: 16.0,
+
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.black),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15.0),
+            child: CircleAvatar(
+              child: Image.network('https://cdn2.iconfinder.com/data/icons/avatars-60/5985/1-Girl-256.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
